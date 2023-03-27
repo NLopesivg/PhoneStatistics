af51176980b4ddf9c72d79bc38834e41092a0467
LOPES Nuno (Iveco Group) <f19705e@ivecoeurope.ivecogroup.iveco.com>
2023-03-27 19:03:29 +0100

    Initial commit

---
+++ src/PhoneStatistics.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+
+public class PhoneStatistics {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path;
+
+        path = fileDialog.createWindow();
+
+        interpretDocument.csvReader(path);
+
+        }
+
+}
---
+++ src/interpretDocument.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class interpretDocument {
+
+    public static void csvReader(String path) throws FileNotFoundException {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(path));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        sc.useDelimiter(";");   //sets the delimiter pattern
+        while (sc.hasNext())  //returns a boolean value
+        {
+            System.out.print(sc.next());  //find and returns the next complete token from this scanner
+        }
+        sc.close();
+    }
+
+}
